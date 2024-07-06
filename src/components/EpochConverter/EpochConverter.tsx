19b2f4358b61d739bcfb98e72451ff84a68ea1d0
omargeo <82685406+omargeo@users.noreply.github.com>
2024-07-06 18:45:56 +0300

    Update EpochConverter.tsx

--- src/components/EpochConverter/EpochConverter.tsx
+++ src/components/EpochConverter/EpochConverter.tsx
@@ -3,7 +3,7 @@ import {Fieldset} from "primereact/fieldset";
 import CurrentEpochTime from "../../containers/EpochConverter/CurrentEpochTime";
 import UnixTimeForm from "../../containers/EpochConverter/UnixTimeForm";
 import DateTimeForm from "../../containers/EpochConverter/DateTimeForm";
- 
+
 const EpochConverter: React.FC = () =>
 
     <Fieldset legend="Unix Timestamp Converter" className="max-w-760 p-mb-6">
